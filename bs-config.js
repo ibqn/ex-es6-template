a17680388e6f947afb119ac2f84034d3cc30edd0
Evgeny Bobkin <evgeny.bobkin@gmail.com>
2017-12-04 17:29:34 +0100

    initial commit

---
+++ bs-config.js
@@ -0,0 +1,23 @@
+'use strict';
+var fallback = require('connect-history-api-fallback');
+var log = require('connect-logger');
+var proxy = require('http-proxy-middleware');
+/*
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ */
+module.exports = {
+  injectChanges: true,
+  files: ['./**/*.{css,js,sass,pug}'],
+  watchOptions: {
+    ignored: 'node_modules'
+  },
+
+  port: 8000,
+  server: {
+    middleware: [
+      // log({ format: '%date %status %method %url' }),
+      proxy('/', { target: 'http://localhost:3000' }),
+    ]
+  }
+};
